5fb00e59e3bd026b95d45f094a78e532ce49839b
Itay Shakury <itay@itaysk.com>
2023-04-06 11:13:58 +0300

    second demo placeholder

---
+++ 2-sign-copy.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+export DEST_REGISTRY=registry.twnt.co
+export DEST_REPO=$DEST_REGISTRY/flasksample
+export DEST_IMAGE=$DEST_REPO:kubeconeu-demo-v1
+echo $DEST_IMAGE
+
+export DEST_PUBKEY=TODO
