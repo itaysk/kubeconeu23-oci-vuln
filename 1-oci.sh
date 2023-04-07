7da82526561fc7e92673ca03b5229d5667eda006
Itay Shakury <itay@itaysk.com>
2023-04-07 13:10:45 +0300

    use trivy plugin to push artifacts

--- 1-oci.sh
+++ 1-oci.sh
@@ -11,11 +11,10 @@ trivy image $SOURCE_IMAGE
 # generate SBOM
 trivy image $SOURCE_IMAGE --format cyclonedx
 # Push SBOM to registry
-trivy image $SOURCE_IMAGE --format cyclonedx | regctl artifact put --subject $SOURCE_IMAGE \
-  --artifact-type application/vnd.cyclonedx+json --file-media-type application/vnd.cyclonedx+json --annotation createdby=trivy
+#trivy plugin install git@github.com:aquasecurity/trivy-plugin-referrer.git
+trivy image $SOURCE_IMAGE --format cyclonedx | trivy referrer put
 # another SBOM
-trivy image $SOURCE_IMAGE --format spdx-json | regctl artifact put --subject $SOURCE_IMAGE \
-  --artifact-type application/spdx+json --file-media-type application/spdx+json --annotation createdby=trivy
+trivy image $SOURCE_IMAGE --format spdx-json | trivy referrer put
 regctl artifact tree $SOURCE_IMAGE
 
 # demonstrate referrers API fallback tag
@@ -23,10 +22,10 @@ regctl tag list $SOURCE_IMAGE
 regctl manifest get "$SOURCE_REPO":TODO
 
 # demonstrate process of fetching SBOM from registry
-regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/spdx+json --filter-annotation createdby=trivy
+regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/cyclonedx+json #--filter-annotation createdby=trivy
 regctl artifact get $SOURCE_IMAGE@TODO
 # fetch SBOM from registry
-regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/spdx+json --filter-annotation createdby=trivy --format '{{ (index .Descriptors 0).Digest  }}' \
+regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/cyclonedx+json --format '{{ (index .Descriptors 0).Digest  }}' \
   | xargs -I {} regctl artifact get $SOURCE_IMAGE@{}
 
 # demonstrate Trivy finds SBOM in registry and uses it for vulnerability scan
@@ -34,4 +33,4 @@ trivy image $SOURCE_IMAGE --sbom-sources oci
 
 # generate SARIF vulnerability report and push to registry
 trivy image $SOURCE_IMAGE --format sarif | regctl artifact put --subject $SOURCE_IMAGE \
-  --artifact-type application/sarif+json --file-media-type application/sarif+json --annotation createdby=trivy
+  --artifact-type application/sarif+json --file-media-type application/sarif+json #--annotation createdby=trivy
