2bd7e8585a02979c7f46a29f844d32c6ba9494fc
Itay Shakury <itay@itaysk.com>
2023-04-07 18:54:04 +0300

    fix sbom push

--- 1-oci.sh
+++ 1-oci.sh
@@ -12,9 +12,9 @@ trivy image $SOURCE_IMAGE
 trivy image $SOURCE_IMAGE --format cyclonedx
 # Push SBOM to registry
 #trivy plugin install git@github.com:aquasecurity/trivy-plugin-referrer.git
-trivy image $SOURCE_IMAGE --format cyclonedx | trivy referrer put
+trivy image $SOURCE_IMAGE --format cyclonedx | trivy referrer put --annotation createdby=trivy
 # another SBOM
-trivy image $SOURCE_IMAGE --format spdx-json | trivy referrer put
+trivy image $SOURCE_IMAGE --format spdx-json | trivy referrer put --annotation createdby=trivy
 regctl artifact tree $SOURCE_IMAGE
 
 # demonstrate referrers API fallback tag
@@ -22,10 +22,10 @@ regctl tag list $SOURCE_IMAGE
 regctl manifest get "$SOURCE_REPO":TODO
 
 # demonstrate process of fetching SBOM from registry
-regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/cyclonedx+json #--filter-annotation createdby=trivy
+regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/vnd.cyclonedx+json --filter-annotation createdby=trivy
 regctl artifact get $SOURCE_IMAGE@TODO
 # fetch SBOM from registry
-regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/cyclonedx+json --format '{{ (index .Descriptors 0).Digest  }}' \
+regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/vnd.cyclonedx+json --format '{{ (index .Descriptors 0).Digest  }}' \
   | xargs -I {} regctl artifact get $SOURCE_IMAGE@{}
 
 # demonstrate Trivy finds SBOM in registry and uses it for vulnerability scan
@@ -33,4 +33,4 @@ trivy image $SOURCE_IMAGE --sbom-sources oci
 
 # generate SARIF vulnerability report and push to registry
 trivy image $SOURCE_IMAGE --format sarif | regctl artifact put --subject $SOURCE_IMAGE \
-  --artifact-type application/sarif+json --file-media-type application/sarif+json #--annotation createdby=trivy
+  --artifact-type application/sarif+json --file-media-type application/sarif+json --annotation createdby=trivy
