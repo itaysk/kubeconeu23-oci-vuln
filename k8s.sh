5f8901f5b39d1605ba9c9973e498647525f83581
Itay Shakury <itay@itaysk.com>
2023-04-12 20:34:01 +0300

    update kyverno image

--- k8s.sh
+++ k8s.sh
@@ -6,7 +6,7 @@ sleep 10
 kubectl create secret docker-registry regcred --docker-server="$DEST_REGISTRY" --docker-username="$DEST_REGISTRY_USER" --docker-password="$DEST_REGISTRY_PASS"
 kubectl patch serviceaccount default --patch '{"imagePullSecrets": [{"name": "regcred"}]}'
 kubectl create -f https://gist.githubusercontent.com/Vishal-Chdhry/73ffec4b0ac8a0ab2c267c09a39f790d/raw/383a746a21b279af2ae7cbd4bb8389c233fe9a14/notation-attestation-install.yaml
-kubectl set image deploy/kyverno-admission-controller -n kyverno kyverno="ghcr.io/vishal-chdhry/kyverno-notary-attestations:demov1.1"
+kubectl set image deploy/kyverno-admission-controller -n kyverno kyverno="ghcr.io/vishal-chdhry/kyverno-notary-attestations:demov1.0"
 kubectl create secret docker-registry -n kyverno regcred --docker-server="$DEST_REGISTRY" --docker-username="$DEST_REGISTRY_USER" --docker-password="$DEST_REGISTRY_PASS"
 kubectl patch deployment kyverno-background-controller -n kyverno --type json --patch '[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--imagePullSecrets=regcred" }]'
 kubectl patch deployment kyverno-admission-controller -n kyverno --type json --patch '[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--imagePullSecrets=regcred" }]'
