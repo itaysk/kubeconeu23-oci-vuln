b0dc0269a265e21d1c8f0c6ed4924e2c7d91dba9
Itay Shakury <itay@itaysk.com>
2023-04-16 21:29:08 +0300

    policy demo updates

--- 3-policy-k8s.sh
+++ 3-policy-k8s.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-kubectl get all
+trivy referrer tree $DEST_IMAGE
 
 # policy - only signed images
+view policy-imagesign.yaml
 kubectl create -f policy-imagesign.yaml
 # succeeds because of signed image
 kubectl run test --image $DEST_IMAGE
@@ -10,17 +11,21 @@ kubectl get pod -w
 kubectl delete pod test
 
 # policy - no GPL dependencies in SBOM
+view policy-licenses.yaml
 kubectl create -f policy-licenses.yaml
 # failes due to GPL dependency
 kubectl run test --image $DEST_IMAGE
 kubectl get pod -w
+kubectl delete -f policy-licenses.yaml
+
 
 # policy - no critical vulnerabilities
+view policy-vulnerabilities.yaml
 kubectl create -f policy-vulnerabilities.yaml
 # fails because of critical vulnerability
 kubectl run test --image $DEST_IMAGE
 # succeeds when using fixed image
-kubectl run test --image $DEST_REPO:fixed
+kubectl run test --image $DEST_IMAGE_FIXED
 kubectl delete pod test
 
 
