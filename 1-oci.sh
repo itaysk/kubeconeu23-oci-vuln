3d1edf79148a81458e21d05aa7e15ec21d1d4e8b
Itay Shakury <itay@itaysk.com>
2023-04-16 12:32:20 +0300

    update oci demo

--- 1-oci.sh
+++ 1-oci.sh
@@ -11,22 +11,22 @@ trivy image $SOURCE_IMAGE --format cyclonedx >/tmp/my.cyclonedx.json
 trivy sbom /tmp/my.cyclonedx.json
 
 # store SBOM in registry
-# trivy image $SOURCE_IMAGE --format cyclonedx | regctl artifact put --subject $SOURCE_IMAGE \
-#   --artifact-type application/vnd.cyclonedx+json --file-media-type application/vnd.cyclonedx+json \
-#   --annotation createdby=trivy --annotation created=$(date -Iseconds)
-# trivy plugin install git@github.com:aquasecurity/trivy-plugin-referrer.git
+trivy image $SOURCE_IMAGE --format cyclonedx | regctl artifact put --subject $SOURCE_IMAGE \
+  --artifact-type application/vnd.cyclonedx+json --file-media-type application/vnd.cyclonedx+json \
+  --annotation "created-by=trivy" --annotation created=$(date -Iseconds)
+trivy plugin install git@github.com:aquasecurity/trivy-plugin-referrer.git
 trivy image $SOURCE_IMAGE --format cyclonedx | trivy referrer put
 
 trivy image $SOURCE_IMAGE --format spdx-json | trivy referrer put
 trivy image $SOURCE_IMAGE --format sarif | trivy referrer put
 
 # list referrering artifacts
-# regctl artifact list $SOURCE_IMAGE
+regctl artifact list $SOURCE_IMAGE
 trivy referrer list $SOURCE_IMAGE --format table
 
 # get contents of specific referring artifact
-# regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/vnd.cyclonedx+json --filter-annotation createdby=trivy | get latest one...
-# regctl artifact get $SOURCE_IMAGE@sha256:...
+regctl artifact list $SOURCE_IMAGE --filter-artifact-type application/vnd.cyclonedx+json --filter-annotation created-by=trivy
+regctl artifact get $SOURCE_IMAGE@sha256:
 trivy referrer get $SOURCE_IMAGE --type cyclonedx
 
 # image scan using SBOM from registry
