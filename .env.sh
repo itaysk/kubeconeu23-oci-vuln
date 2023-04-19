c99a46ad205b381feb5bceb6a64f070b4e5893af
Itay Shakury <itay@itaysk.com>
2023-04-19 15:11:16 +0200

    remove .env

--- .env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export SOURCE_REGISTRY=ghcr.io
-export SOURCE_REPO=$SOURCE_REGISTRY/toddysm/cssc-pipeline/flasksample
-export SOURCE_IMAGE=$SOURCE_REPO:kubeconeu-demo-v1
-
-export DEST_REGISTRY=registry.twnt.co
-export DEST_REPO=$DEST_REGISTRY/flasksample
-export DEST_IMAGE=$DEST_REPO:kubeconeu-demo-v1
